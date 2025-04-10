123af7caf17f5fe1ac640f802204b489476eae0c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-10 18:04:59 +0200

    renamed interfaces

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { ContactInterface } from '../interfaces/contact-interface';
+import { ContactInterface } from '../interfaces/contact.interface';
 import { Firestore, collection, doc, onSnapshot, collectionData, addDoc, updateDoc, deleteDoc, query, where, orderBy, limit } from '@angular/fire/firestore';
 
 @Injectable({
