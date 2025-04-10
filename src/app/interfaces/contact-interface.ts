12a1213ce85faa478f735f7305daca443d90c427
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-10 14:27:02 +0200

    connected firebase

--- src/app/interfaces/contact-interface.ts
+++ src/app/interfaces/contact-interface.ts
@@ -5,3 +5,18 @@ export interface ContactInterface {
         mail: string;
 
 }
+
+/* 
+firebase-tools:
+npm install -g firebase-tools@13.0.1
+
+AngularFire:
+ng add @angular/fire@17.0.0
+
+
+{
+  "projects": {
+    "default": "join-app"
+  }
+}
+*/
