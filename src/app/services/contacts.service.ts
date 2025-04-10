5b0517e2d827a27c05ef636cda2ed3835df84784
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-10 15:04:25 +0200

    added firebase connection on init to contact service

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { ContactInterface } from '../interfaces/contact-interface';
+import { Firestore, collection, doc, onSnapshot, collectionData, addDoc, updateDoc, deleteDoc, query, where, orderBy, limit } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  contacts: ContactInterface[] = [];
+  unsubContact;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() { 
+
+    this.unsubContact = onSnapshot(this.getContactsRef(), (list)=> {
+      list.forEach((doc) => {
+        console.log(doc);
+      })
+    });
+
+
+  } // constructor end
+
+  ngOnDestroy() {
+    this.unsubContact();
+  }
+
+  getContactsRef() {
+    return collection(this.firestore, 'contacts');
+  }
 }
+
