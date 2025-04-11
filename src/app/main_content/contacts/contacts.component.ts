97b9716a57e1f7341df80c0d6e754ae6c516e8f7
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-11 17:14:12 +0200

    Implemented delete contact to contacts component

--- src/app/main_content/contacts/contacts.component.ts
+++ src/app/main_content/contacts/contacts.component.ts
@@ -3,16 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ContactDialogComponent } from './contact-dialog/contact-dialog.component';
 import { ContactsService } from '../../services/contacts.service';
 
-
 @Component({
   selector: 'app-contacts',
   standalone: true,
   imports: [ContactDialogComponent, CommonModule],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
 export class ContactsComponent {
-
   contactsService = inject(ContactsService);
 
   showDialog = false;
@@ -25,5 +23,13 @@ export class ContactsComponent {
     this.showDialog = false;
   }
 
-
+  async deleteContact() {
+    const docId = '1JZx6aGq41MRooCAOS0P'; //Platzhalter, existiert jetzt nicht mehr :)
+    try {
+      await this.contactsService.deleteContact(docId);
+      console.log('Kontakt erfolgreich gelöscht');
+    } catch (error) {
+      console.error('Fehler beim Löschen des Kontakts:', error);
+    }
+  }
 }
