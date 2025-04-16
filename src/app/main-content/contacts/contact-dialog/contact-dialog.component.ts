a5bd96a2f4fce5f2c1ba150ab6b2c2a3ffa5a529
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-04-16 02:38:51 +0200

    prevent duplicate contact creation by validating name and email

--- src/app/main-content/contacts/contact-dialog/contact-dialog.component.ts
+++ src/app/main-content/contacts/contact-dialog/contact-dialog.component.ts
@@ -11,6 +11,7 @@ import {
 import { FormsModule } from '@angular/forms';
 import { ContactInterface } from '../../../interfaces/contact.interface';
 import { ContactsService } from '../../../services/contacts.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-dialog',
@@ -37,6 +38,9 @@ export class ContactDialogComponent implements OnInit, OnDestroy {
 
   animateIn = false;
   animateOut = false;
+
+  nameExists = false;
+  mailExists = false;
 
   contactData: ContactInterface = {
     name: '',
@@ -75,7 +79,7 @@ export class ContactDialogComponent implements OnInit, OnDestroy {
   }
 
 
-  // // Simulierter Fehler beim Erstellen
+  // // Simulierter Fehler beim Erstellen ^^
 
   //   onCreate(index: number | undefined): void {
   //     const { name, mail } = this.contactData;
@@ -102,12 +106,37 @@ export class ContactDialogComponent implements OnInit, OnDestroy {
   //     }
   //   }
 
-  onCreate(index: number | undefined): void {
-    const { name, mail } = this.contactData;
-    if (!name.trim() || !mail.trim()) return;
+  onCreate(index: number | undefined, contactForm: NgForm): void {
+    this.resetValidation();
+    if (this.isInvalidForm(contactForm) || this.doubleCheckData(index)) return;
     index == null ? this.createNewContact() : this.editContact(index);
   }
-  
+
+  resetValidation() {
+    this.nameExists = false;
+    this.mailExists = false;
+  }
+
+  isInvalidForm(form: NgForm): boolean {
+    if (form.invalid) {
+      Object.values(form.controls).forEach(c => c?.markAsTouched());
+      return true;
+    }
+    return false;
+  }
+
+  doubleCheckData(index?: number): boolean {
+    const dupe = this.contactsService.contacts.find((c, i) => i !== index && (
+      c.name.toLowerCase() === this.contactData.name.toLowerCase() ||
+      c.mail.toLowerCase() === this.contactData.mail.toLowerCase()
+    ));
+    if (!dupe) return false;
+    this.nameExists = dupe.name.toLowerCase() === this.contactData.name.toLowerCase();
+    this.mailExists = dupe.mail.toLowerCase() === this.contactData.mail.toLowerCase();
+    return true;
+  }
+
+
   createNewContact(): void {
     this.contactData.color ||= this.contactsService.contactColors[
       Math.floor(Math.random() * this.contactsService.contactColors.length)
@@ -116,7 +145,7 @@ export class ContactDialogComponent implements OnInit, OnDestroy {
       .then(() => { this.create.emit(); this.onCancel(); })
       .catch(() => this.error.emit());
   }
-  
+
 
 
   async editContact(index: number) {
