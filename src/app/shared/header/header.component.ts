39cefd54f4d095cc34ca99144f90827e0a185eca
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-16 15:30:44 +0200

    Changed dropdown menu at widescreen to logout button

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,21 +6,74 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  isDropdownOpen = false;
+  showDropdown = false;
+  dropdownVisible = false;
+  showLogoutPopup = false;
+  logoutPopupVisible = false; // Steuert, ob das Element im DOM ist
+  isWideScreen = window.innerWidth > 1920;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isWideScreen = event.target.innerWidth > 1920;
+    if (!this.isWideScreen) {
+      this.showLogoutPopup = false;
+    } else {
+      this.showDropdown = false;
+    }
+  }
+
+  onGButtonClick(event: MouseEvent) {
+    event.stopPropagation();
+    if (this.isWideScreen) {
+      this.togglePopup();
+      this.closeDropdown();
+    } else {
+      this.toggleDropdown();
+      this.closeLogoutPopup();
+    }
+  }
+
+  togglePopup() {
+    if (this.logoutPopupVisible) {
+      this.closeLogoutPopup();
+    } else {
+      this.openLogoutPopup();
+    }
+  }
+
+  openLogoutPopup() {
+    this.logoutPopupVisible = true;
+    setTimeout(() => (this.showLogoutPopup = true), 10);
+  }
+
+  closeLogoutPopup() {
+    this.showLogoutPopup = false;
+    setTimeout(() => (this.logoutPopupVisible = false), 200);
+  }
 
   toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
+    if (this.dropdownVisible) {
+      this.closeDropdown();
+    } else {
+      this.openDropdown();
+    }
   }
-  
-  @HostListener('document:click')
-  handleClickOutside(): void {
-    this.isDropdownOpen = false;
+
+  openDropdown() {
+    this.dropdownVisible = true;
+    setTimeout(() => (this.showDropdown = true), 10);
+  }
+
+  closeDropdown() {
+    this.showDropdown = false;
+    setTimeout(() => (this.dropdownVisible = false), 300);
   }
 
   logout() {
     // Logout-Logik
+    this.showLogoutPopup = false;
   }
 }
