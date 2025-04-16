08c9e2c321dc4ad5f62253498d27cfeb9531ee20
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-16 09:09:53 +0200

    added missing pathts to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,15 @@ import { ContactsComponent } from './main-content/contacts/contacts.component';
 import { HelpComponent } from './main-content/help/help.component';
 import { LegalNoticeComponent } from './main-content/legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './main-content/privacy-policy/privacy-policy.component';
+import { AddTaskComponent } from './main-content/add-task/add-task.component';
+import { BoardComponent } from './main-content/board/board.component';
 
 export const routes: Routes = [
     { path: '', component: ContactsComponent },
     { path: 'contacts', component: ContactsComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'addtask', component: AddTaskComponent },
+    { path: 'board', component: BoardComponent },
     { path: 'help', component: HelpComponent },
     { path: 'legal-notice', component: LegalNoticeComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent }
