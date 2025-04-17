546331761ac2fedce92722cc035c1e28eed4ffcc
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-17 15:52:28 +0200

    Tidy up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { SummaryComponent } from './main-content/summary/summary.component';
-// import { ContactsComponent } from './main-content/contacts/contacts.component';
 import { HeaderComponent } from "./shared/header/header.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
-// import { ContactsService } from './services/contacts.service';
-// import { HelpComponent } from './main-content/help/help.component';
 
 @Component({
   selector: 'app-root',
