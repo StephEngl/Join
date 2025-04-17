7a19b0747f912a4b5a00611ce1d64b5e1a5750f0
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-17 15:07:31 +0200

    Adapted styles help and router section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SummaryComponent } from './main-content/summary/summary.component';
-import { ContactsComponent } from './main-content/contacts/contacts.component';
+// import { SummaryComponent } from './main-content/summary/summary.component';
+// import { ContactsComponent } from './main-content/contacts/contacts.component';
 import { HeaderComponent } from "./shared/header/header.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { ContactsService } from './services/contacts.service';
+// import { ContactsService } from './services/contacts.service';
+// import { HelpComponent } from './main-content/help/help.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SummaryComponent, ContactsComponent, HeaderComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
