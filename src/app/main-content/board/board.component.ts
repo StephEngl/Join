33407398cf1162d803fa123b59d5a18ac0e83287
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-23 14:42:34 +0200

    Added nameInitial function to task firebase service

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksService } from '../../services/tasks.service';
 import { TaskFirebaseTempComponent } from './task-firebase-temp/task-firebase-temp.component';
+import { TaskComponent } from './task/task.component';
 
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, TaskFirebaseTempComponent],
+  imports: [CommonModule, TaskFirebaseTempComponent, TaskComponent],
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
