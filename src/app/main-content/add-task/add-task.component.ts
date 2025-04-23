25a2448f2e9015944f8b339713adbf29b620a511
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-23 14:41:37 +0200

    Add form to task-section

--- src/app/main-content/add-task/add-task.component.ts
+++ src/app/main-content/add-task/add-task.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
+  today: string = new Date().toISOString().split('T')[0];
 
+  subtaskText = '';
+
+  clearForm() {
+    
+  }
+
+  onInputChange() {
+    // Optional: Validierung oder weitere Logik
+  }
+
+  addSubtask() {
+    if (this.subtaskText.trim()) {
+      // Hier Subtask zur Liste hinzufügen
+      this.subtaskText = '';
+    }
+  }
+
+  clearSubtask() {
+    this.subtaskText = '';
+  }
+
+  focusInput(input: HTMLInputElement) {
+    input.focus();
+  }
 }
