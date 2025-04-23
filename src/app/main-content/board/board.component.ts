317b9199a535e660df6c4dc2324537f390d755df
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-04-23 12:50:01 +0200

    Merge conflict resolved in board components – board structure & task progress saved

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { TasksService } from '../../services/tasks.service';
 import { TaskFirebaseTempComponent } from './task-firebase-temp/task-firebase-temp.component';
 
+
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [CommonModule, TaskFirebaseTempComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+
   tasksService = inject(TasksService);
+
 }
