d413904c5d0b6b989ccb38bd0bd174ca9bbd1380
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-24 14:05:12 +0200

    Added drag & drop function to board

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -4,12 +4,22 @@ import { TasksService } from '../../services/tasks.service';
 import { TaskFirebaseTempComponent } from './task-firebase-temp/task-firebase-temp.component';
 import { TaskComponent } from './task/task.component';
 import { TaskInterface } from '../../interfaces/task.interface';
-
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, TaskFirebaseTempComponent, TaskComponent],
+  imports: [
+    CommonModule,
+    TaskFirebaseTempComponent,
+    TaskComponent,
+    DragDropModule
+  ],
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
@@ -27,9 +37,25 @@ export class BoardComponent {
     return this.tasksService.tasks.filter(task => task.taskType === status);
   }
   
+  // extracting each taskStatus from boardColumns
+  connectedDropLists = this.boardColumns.map(col => col.taskStatus);
+
+  //cdk drag & drop method connected to firebase
+  drop(event: CdkDragDrop<TaskInterface[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      const task = event.previousContainer.data[event.previousIndex];
+      task.taskType = event.container.id as TaskInterface['taskType'];
+      this.tasksService.updateTask(task);
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
   
-  
-
-
-
 }
