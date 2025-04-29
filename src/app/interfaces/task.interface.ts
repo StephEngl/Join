3b25e41d68dd63a152c85acf27e1a8c1eb2b6bfe
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-29 13:42:25 +0200

    Add null to duedate taskinterface for placeholder

--- src/app/interfaces/task.interface.ts
+++ src/app/interfaces/task.interface.ts
@@ -1,11 +1,11 @@
 export interface TaskInterface {
-    id?: string;
-    title: string;
-    description: string;
-    category: "Technical Task" | "User Story" | undefined;
-    dueDate: Date;
-    priority: "urgent" | "medium" | "low";
-    taskType: "toDo" | "done" | "inProgress" | "feedback"; 
-    subTasks: any[]; 
-    assignedTo: any[];
+  id?: string;
+  title: string;
+  description: string;
+  category: 'Technical Task' | 'User Story' | undefined;
+  dueDate: Date | null;
+  priority: 'urgent' | 'medium' | 'low';
+  taskType: 'toDo' | 'done' | 'inProgress' | 'feedback';
+  subTasks: any[];
+  assignedTo: any[];
 }
