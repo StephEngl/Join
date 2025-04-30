556b009ea04e1dc0883d50d6b0ac495c44fd5906
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-04-30 11:00:47 +0200

    Added host to task details

--- src/app/main-content/add-task/task-details/task-details.component.ts
+++ src/app/main-content/add-task/task-details/task-details.component.ts
@@ -15,67 +15,13 @@ import { SingleTaskDataService } from '../../../services/single-task-data.servic
 @Component({
   selector: 'app-task-details',
   standalone: true,
+  host: { 'class': 'task-details' },
   imports: [FormsModule, CdkAccordionModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.scss',
 })
 export class TaskDetailsComponent {
 
-  // @Input() taskCategories!: any[];
-  // @Input() hoveredContact!: any;
-  // @Input() mouseX!: number;
-  // @Input() mouseY!: number;
-
-  // @Input() subtaskText!: string;
-  // @Output() subtaskTextChange = new EventEmitter<string>();
-
-  // @Input() subtasksContainer!: {
-  //   text: string;
-  //   isEditing: boolean;
-  //   isHovered: boolean;
-  //   isChecked: boolean;
-  // }[];
-  // @Output() subtasksContainerChange = new EventEmitter<any[]>();
-
-  // @Input() priorityButtons!: {
-  //   imgInactive: string;
-  //   imgActive: string;
-  //   colorActive: string;
-  //   priority: 'Urgent' | 'Medium' | 'Low';
-  //   btnActive: boolean;
-  // }[];
-  // @Output() priorityButtonsChange = new EventEmitter<any[]>();
-
-  // @Input() assignedTo!: any[];
-  // @Output() assignedToChange = new EventEmitter<any[]>();
-
-  // @Input() selectedCategory!: string | undefined;
-  // @Output() selectedCategoryChange = new EventEmitter<string | undefined>();
-
-  // // Für Subtask-Edit
-  // inputFieldSubT: string = '';
-  // @ViewChild('inputFieldSubTask') inputFieldSubTaskRef!: ElementRef;
-
-  // // Events für Methoden
-  // @Output() inputChange = new EventEmitter<void>();
-
-  // @Output() priorityChange = new EventEmitter<number>();
-  // @Output() assignedContactsChange = new EventEmitter<any>();
-  // @Output() removeAssignedContact = new EventEmitter<string>();
-  // @Output() categoryChange = new EventEmitter<number>();
-  // @Output() addSubtask = new EventEmitter<void>();
-  // @Output() clearSubtask = new EventEmitter<void>();
-  // @Output() editSubtask = new EventEmitter<any>();
-  // @Output() editCheckSubtask = new EventEmitter<any>();
-  // @Output() deleteSubtask = new EventEmitter<any>();
-
-  // Priority-Button click
-  setPriority(index: number) {
-    this.taskData.priorityButtons.forEach((btn, i) => btn.btnActive = i === index ? !btn.btnActive : false);
-  }
-
-  // data from service single-task-data 
-  // Work in Progress from here
   mouseX: number = 0;
   mouseY: number = 0;
   contactsService = inject(ContactsService);
@@ -99,6 +45,10 @@ export class TaskDetailsComponent {
         .toLowerCase()
         .includes(this.searchedContactName.toLowerCase())
     );
+  }
+
+  setPriority(index: number) {
+    this.taskData.priorityButtons.forEach((btn, i) => btn.btnActive = i === index ? !btn.btnActive : false);
   }
 
   toggleAssignedContacts(contactId: any) {
@@ -135,7 +85,9 @@ export class TaskDetailsComponent {
   }
 
   removeAssignedContact(contactId: string): void {
-    this.taskData.assignedTo = this.taskData.assignedTo.filter((id) => id !== contactId);
+    this.taskData.assignedTo = this.taskData.assignedTo.filter(
+      (c) => c.contactId !== contactId
+    );
     this.hoveredContact = undefined;
   }
 
