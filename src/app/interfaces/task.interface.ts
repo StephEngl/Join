0c459913205117ef52b2efd54a37062f75981fe2
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-02 08:31:06 +0200

    Adjusting styling of hover effect & columns in board.component

--- src/app/interfaces/task.interface.ts
+++ src/app/interfaces/task.interface.ts
@@ -8,19 +8,4 @@ export interface TaskInterface {
   taskType: 'toDo' | 'done' | 'inProgress' | 'feedback';
   subTasks: any[];
   assignedTo: any[];
-
-  // NOTE: No changes needed for manual dialog switch.
-  // Interface remains unchanged – data is passed via @Input in TaskDialogComponent.
 }
-
-// export interface TaskInterface {
-//   id?: string;
-//   title: string;
-//   description: string;
-//   category: 'Technical Task' | 'User Story' | undefined;
-//   dueDate: Date | null;
-//   priority: 'urgent' | 'medium' | 'low';
-//   taskType: 'toDo' | 'done' | 'inProgress' | 'feedback';
-//   subTasks: any[];
-//   assignedTo: any[];
-// }
