9cad46dd22b4ce29b5710540d62d79ccf3110b71
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-02 14:23:40 +0200

    Delete obsolete code from task dialog

--- src/app/main-content/board/task-dialog/task-dialog.component.ts
+++ src/app/main-content/board/task-dialog/task-dialog.component.ts
@@ -4,58 +4,47 @@ import { TaskInterface } from '../../../interfaces/task.interface';
 import { TaskInfoComponent } from './task-info/task-info.component';
 import { AddTaskComponent } from '../../add-task/add-task.component';
 import { TasksService } from '../../../services/tasks.service';
-// import { MatDialogRef } from '@angular/material/dialog'; /* Removed: using manual close */
-// import { MAT_DIALOG_DATA } from '@angular/material/dialog'; /* Removed: using manual input */
 import { SingleTaskDataService } from '../../../services/single-task-data.service';
 
 @Component({
-    selector: 'app-task-dialog',
-    templateUrl: './task-dialog.component.html',
-    styleUrls: ['./task-dialog.component.scss'],
-    standalone: true,
-    imports: [
-        CommonModule,
-        TaskInfoComponent,
-        AddTaskComponent
-    ]
+  selector: 'app-task-dialog',
+  templateUrl: './task-dialog.component.html',
+  styleUrls: ['./task-dialog.component.scss'],
+  standalone: true,
+  imports: [CommonModule, TaskInfoComponent, AddTaskComponent],
 })
 export class TaskDialogComponent {
+  taskDataService = inject(SingleTaskDataService);
+  @Input() taskDataDialog!: TaskInterface;
 
-    taskDataService = inject(SingleTaskDataService);
-    @Input() taskDataDialog!: TaskInterface;
+  @Output() close = new EventEmitter<void>();
 
-@Output() close = new EventEmitter<void>(); // ❗️ hinzugefügt
+  constructor(private tasksService: TasksService) {}
 
-    constructor(
-        private tasksService: TasksService,
-        // private dialogRef: MatDialogRef<TaskDialogComponent>,  /* Commented out */
-        // @Inject(MAT_DIALOG_DATA) public data: TaskInterface     /* Commented out */
-    ) {}
+  showTaskInfo: boolean = true;
 
-    showTaskInfo: boolean = true;
+  onEditTask(): void {
+    this.taskDataService.editModeActive = true;
+    setTimeout(() => {
+      this.showTaskInfo = false;
+    }, 10);
+  }
 
-    onEditTask(): void {
-        this.taskDataService.editModeActive = true;
-        setTimeout(() => {
-            this.showTaskInfo = false;
-        }, 10);
+  onCancelEditTask(): void {
+    this.showTaskInfo = true;
+    this.taskDataService.editModeActive = false;
+  }
+
+  /* Replaced: this.dialogRef.close() */
+  closeDialog(): void {
+    this.close.emit();
+    this.taskDataService.editModeActive = false;
+  }
+
+  async deleteTask(): Promise<void> {
+    if (this.taskDataDialog && this.taskDataDialog.id) {
+      await this.tasksService.deleteTask(this.taskDataDialog.id);
+      this.closeDialog();
     }
-
-    onCancelEditTask(): void {
-        this.showTaskInfo = true;
-        this.taskDataService.editModeActive = false;
-    }
-
-    /* Replaced: this.dialogRef.close() */
-    closeDialog(): void {
-        this.close.emit(); // ❗️ statt dialogRef.close()
-        this.taskDataService.editModeActive = false;
-    }
-
-    async deleteTask(): Promise<void> {
-        if (this.taskDataDialog && this.taskDataDialog.id) {
-            await this.tasksService.deleteTask(this.taskDataDialog.id);
-            this.closeDialog();
-        }
-    }
+  }
 }
