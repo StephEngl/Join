438f2addc51d2be6f0d3483e26b859e1759b2087
Jonathan Michutta <jon.mic@joz-artworks.de>
2025-05-02 14:10:25 +0200

    ✨🐛 feat: Show toast on task creation and redirect to board
    Added a success toast with board icon when a new task is created via AddTaskComponent. User is now automatically redirected to the board after task creation with a short delay.

--- src/app/shared/toast/toast.service.ts
+++ src/app/shared/toast/toast.service.ts
@@ -7,10 +7,12 @@ export class ToastService {
     toastType$ = new BehaviorSubject<'create' | 'update' | 'delete' | 'error'>('create');
     isToastOpen$ = new BehaviorSubject<boolean>(false);
     isToastAnimated$ = new BehaviorSubject<boolean>(false);
-    
-    triggerToast(message: string, type: 'create' | 'update' | 'delete' | 'error') {
+    toastIcon$ = new BehaviorSubject<string>('');
+
+    triggerToast(message: string, type: 'create' | 'update' | 'delete' | 'error', iconPath: string = '') {
         this.toastMessage$.next(message);
         this.toastType$.next(type);
+        this.toastIcon$.next(iconPath);
         this.isToastOpen$.next(true);
         setTimeout(() => this.isToastAnimated$.next(true), 10);
         setTimeout(() => this.isToastAnimated$.next(false), 2000);
