86dc86157e0d89376e8021f4b83f17ab7845b098
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-07 13:15:58 +0200

    Working on login.component & login,dialog, added start-animation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,46 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { SignalsService } from '../services/signals.service';
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+import { SignupDialogComponent } from './signup-dialog/signup-dialog.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginDialogComponent, SignupDialogComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   signalService = inject(SignalsService);
+  loginLogoHeight: string = "100%";
+  loginLogoWidth: string = "100%";
+  loginBackgroundColor: string = "";
+  loginLogoImgSrc: string = "./assets/icons/header/logo.svg";
+
   constructor(private router: Router) {}
 
   toMain() {
     this.signalService.dummyAuthStatus.set(true);
     this.router.navigate(['/summary']);
   }
+
+  ngOnInit() {
+    if (this.signalService.isMobile()) {
+      this.loginBackgroundColor= "#2A3647";
+      this.loginLogoImgSrc = "./assets/icons/header/logo_white.svg";
+    } else {
+      this.loginBackgroundColor= "#F6F7F8";
+    }
+    setTimeout(() => {
+      this.loginBackgroundColor = 'transparent';
+    }, 200);
+    setTimeout(() => {
+      this.loginLogoHeight = '120px';
+      this.loginLogoWidth = "100px";
+      this.loginLogoImgSrc = "./assets/icons/header/logo.svg";
+    }, 600);
+  }
+  
 
 }
