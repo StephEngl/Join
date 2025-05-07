5efb4968e6f20342269cf1e3b9bfda4dd5c913e8
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-07 13:34:47 +0200

    Add dialog content

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -37,6 +37,4 @@ export const appConfig: ApplicationConfig = {
         importProvidersFrom(MatDialogModule), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"join-3324b","appId":"1:445334637484:web:3cfea01c4d00e60b298809","storageBucket":"join-3324b.firebasestorage.app","apiKey":"AIzaSyDVgj9vu_mfnWLJ7-54tVT8WnejpVGJVEU","authDomain":"join-3324b.firebaseapp.com","messagingSenderId":"445334637484"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))
         
     ]
-
-    
 };
