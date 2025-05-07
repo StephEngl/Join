9669e3cdab5c2efb8a2671c094b0a3180b66c892
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-07 16:51:34 +0200

    Implement user interface and ng form to login dialog

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -1,2 +1,4 @@
 export interface UserInterface {
+  email: '';
+  password: '';
 }
--- src/app/login/login-dialog/login-dialog.component.ts
+++ src/app/login/login-dialog/login-dialog.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserInterface } from '../../interfaces/user.interface';
+
 
 @Component({
   selector: 'app-login-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login-dialog.component.scss'
 })
 export class LoginDialogComponent {
+formSubmitted = false;
+loginData: UserInterface = {
+  email: '',
+  password: '',
+};
 
 }
