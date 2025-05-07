1721926e15bcf46363cf9612354971b621629ffb
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-07 11:58:05 +0200

    Built dummy auth, showing up login page after init

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { SignalsService } from '../services/signals.service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  signalService = inject(SignalsService);
+  constructor(private router: Router) {}
+
+  toMain() {
+    this.signalService.dummyAuthStatus.set(true);
+    this.router.navigate(['/summary']);
+  }
 
 }
