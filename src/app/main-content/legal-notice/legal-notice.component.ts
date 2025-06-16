6fece37fd2938c4b575de5dc2751247e1b7de7d3
Stephanie Englberger <stephaniekoebich@aol.com>
2025-06-16 14:42:42 +0200

    Adapt legal-notice and privacy-policy to my conditions

--- src/app/main-content/legal-notice/legal-notice.component.ts
+++ src/app/main-content/legal-notice/legal-notice.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AddTaskComponent } from "../add-task/add-task.component";
 
 /**
  * Component for displaying the legal notice page.
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [AddTaskComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
