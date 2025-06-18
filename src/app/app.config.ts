7b85ee029b42a785d6d8832cdb9ff81d4cd8e3fb
Stephanie Englberger <stephaniekoebich@aol.com>
2025-06-18 12:58:54 +0200

    Add environments file for gitignore firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,36 +10,25 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 
-export const appConfig: ApplicationConfig = {
-    providers: [
-        /* Provides routing for the application */
-        provideRouter(routes),
+import { environment } from '../environments/environments'
 
-        /* Initializes Firebase with environment-specific config */
-        importProvidersFrom(provideFirebaseApp(() =>
-            initializeApp({
-                projectId: "join-3324b",
-                appId: "1:445334637484:web:3cfea01c4d00e60b298809",
-                storageBucket: "join-3324b.firebasestorage.app",
-                apiKey: "AIzaSyDVgj9vu_mfnWLJ7-54tVT8WnejpVGJVEU",
-                authDomain: "join-3324b.firebaseapp.com",
-                messagingSenderId: "445334637484"
-            })
-        )),
+export const appConfig: ApplicationConfig = {
+  providers: [
+    /* Provides routing for the application */
+        provideRouter(routes),
 
         /* Provides Firestore database access */
         importProvidersFrom(provideFirestore(() => getFirestore())),
+        importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
 
         /* Provides Firebase Authentication (required for inject(Auth)) */
-        importProvidersFrom(provideAuth(() => getAuth())), /* <-- REQUIRED for SignUpComponent */
+        importProvidersFrom(provideAuth(() => getAuth())),
 
         /* Enables animations (noop if disabled) */
         provideAnimationsAsync('noop'),
         provideAnimations(),
 
         /* Provides Angular Material Dialog globally */
-        importProvidersFrom(MatDialogModule)
-
-        /* NOTE: Nothing has been removed from the original setup */
-    ]
+        importProvidersFrom(MatDialogModule),
+  ],
 };
