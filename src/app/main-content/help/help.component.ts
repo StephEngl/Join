d11f8f641cbfdaeb6114d231a3c90a8bf95e2617
Stephanie Englberger <stephaniekoebich@aol.com>
2025-07-28 23:02:22 +0200

    Implements go-back-button to help-section

--- src/app/main-content/help/help.component.ts
+++ src/app/main-content/help/help.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-help',
   standalone: true,
   imports: [],
   templateUrl: './help.component.html',
-  styleUrl: './help.component.scss'
+  styleUrl: './help.component.scss',
 })
 export class HelpComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }
